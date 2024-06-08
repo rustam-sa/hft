0f7ce95803a3e132b10cd94d867109ef7a72fda2
Rustam <rustam308@gmail.com>
2024-06-08 01:22:54 -0400

    Make progress, should be able to create training data after next update

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Float, BigInteger, String
+from sqlalchemy import Column, Integer, Float, BigInteger, String, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -16,3 +16,13 @@ class MarketData(Base):
     low = Column(Float, nullable=False)
     volume = Column(Float, nullable=False)
     amount = Column(Float, nullable=False)
+
+
+class CandlestickImage(Base):
+    __tablename__ = 'candlestick_images'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    sample_id = Column(Integer, nullable=False)
+    collection_name = Column(String, nullable=False)
+    timestamp = Column(Integer, nullable=False)
+    image_data = Column(LargeBinary, nullable=False)
+    label = Column(Integer, nullable=False)
